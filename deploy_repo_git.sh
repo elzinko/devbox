5a24cf5d1f35ae563c62f46f9b7e65273ed41a12
Thomas Couderc <thomas.couderc@gmail.com>
2014-12-11 00:01:57 +0100

    update deploy from git

--- deploy_repo_git.sh
+++ deploy_repo_git.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-docker run -t --rm -v /src/vertx/:/usr/local/src vertxdev git clone https://github.com/elzinko/mystore.git
+docker run -t --rm -v /Users/elzinko/Devs/projects/devbox/src/:/usr/local/src devbox git clone https://github.com/elzinko/mystore.git
